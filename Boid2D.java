9ca4e4d5be2514a290d5333415b0a5ad79651ee3
Ian Spooner <ispooner@gmail.com>
2015-11-13 14:18:32 +0900

    Successfully changed boids to use vector notation.
    Boids now use vectors and not angles. This makes it much easier to
    implement more steering behaviors. I want to get basic steering
    behaviors first before I start implementing the main purpose of this
    project.

--- Boid2D.java
+++ Boid2D.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 
 import javafx.geometry.Point2D;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
 
@@ -22,6 +23,7 @@ public class Boid2D{
 	private final double maxForce = 3.0; 	//This value is used to determine how much effect the total forces have on the boid.
 											//This value may be split up so that each force may have a different "weight"
 	
+	private Point2D bounds;		//The bounding frame. Used to wrap the boids back onto the screen Asteroids style.
 	private Point2D velocity;	//The current direction and speed of this boid.
 	private Point2D steering;	//The amount of change added to the velocity on each frame.
 	private Point2D seekForce;	//The current seek force for this boid. It will seek the center of mass of the boids around it.
@@ -31,7 +33,7 @@ public class Boid2D{
 	//The random wander force of each boid. This will likely be the only random force directly acting on the boids.
 	private Point2D wanderForce;
 	private double wanderAngle; //Need to store this so the boid doesn't twitch randomly.
-	private final double angleChange = 10; //Determines how much the wanderAngle can change between frames. Tweaking this value will produce more realistic wandering behavior.
+	private final double angleChange = 15; //Determines how much the wanderAngle can change between frames. Tweaking this value will produce more realistic wandering behavior.
 	
 	private double close; 		//Used to determine how close another boid is. The closer a boid, the effect it will have on this boid.
 								/*
@@ -43,33 +45,158 @@ public class Boid2D{
 	
 	private BoidType type;		//Used to define the behavior of this boid. Behaviors are defined above.
 	
+	//getters for all the variables I need to get.
+	public Shape getShape()
+	{
+		return shape;
+	}
+	
+	public Point2D getVelocity()
+	{
+		return velocity;
+	}
+	
+	public Point2D getPosition()
+	{
+		return new Point2D(shape.getTranslateX(), shape.getTranslateY());
+	}
+	
+	//Setters for variables.
+	public void setVelocity(Point2D v)
+	{
+		//I am checking to make sure that the boid will continue to move within the speed limits.
+		if(v.magnitude() < minSpeed)
+		{
+			v = v.normalize().multiply(minSpeed);
+		}
+		if(v.magnitude() > maxSpeed)
+		{
+			v = v.normalize().multiply(maxSpeed);
+		}
+		
+		velocity = v;
+		
+		shape.setRotate(Math.toDegrees(Math.atan2(velocity.getX(), -velocity.getY())));
+	}
+	
+	public void setPosition(Point2D p)
+	{
+		shape.setTranslateX(p.getX() % bounds.getX());
+		shape.setTranslateY(p.getY() % bounds.getY());
+		if(shape.getTranslateX() < 0)
+		{
+			shape.setTranslateX(bounds.getX());
+		}
+		
+		if(shape.getTranslateY() < 0)
+		{
+			shape.setTranslateY(bounds.getY());;
+		}
+	}
+	
 	//Initialize this boids values to the given parameters
-	public Boid2D(Shape s, double maxS, double minS, Point2D v, BoidType t)
+	public Boid2D(Shape s, double maxS, double minS, Point2D b, Point2D v, Point2D p, BoidType t, Paint c)
 	{
 		shape = s;
 		maxSpeed = maxS;
 		minSpeed = minS;
-		velocity = v;
+		bounds = b;
+		setVelocity(v);
+		setPosition(p);
 		type = t;
+		shape.setFill(c);
 		
 		steering = Point2D.ZERO;
 		seekForce = Point2D.ZERO;
 		fleeForce = Point2D.ZERO;
 		avoidForce = Point2D.ZERO;
 		wanderForce = Point2D.ZERO;
-		wanderAngle = velocity.angle(0, 1);
+		wanderAngle = velocity.angle(0, 1); //I set the wanderAngle to the angle of the velocity so the boid moves straight at first.
 		
 	}
 	
-	public double averageAngles(Double[] angles)
+	/*
+	 * The move function will take the array of boids and calculate this boid's movement based on the positions and
+	 * velocities of the boids around it. This function will be called on all boids. 
+	 */
+	public void move(ArrayList<Boid2D> boids)
 	{
-		double x = 0, y = 0;
-		for(int i = 0; i < angles.length; i++)
+		Boid2D[] c = closeBoids(boids);
+		steering = Point2D.ZERO;
+		steering = steering.add(seek(c));
+		steering = steering.add(flee(c));
+		steering = steering.add(avoid(c));
+		steering = steering.add(wander());
+		
+		if(steering.magnitude() > maxForce)
 		{
-			x += Math.cos(Math.toRadians(angles[i]));
-			y += Math.sin(Math.toRadians(angles[i]));
+			steering = steering.normalize().multiply(maxForce);
 		}
 		
-		return Math.toDegrees(Math.atan2(y, x));
+		setVelocity(getVelocity().add(steering));
+		
+		setPosition(getPosition().add(velocity));
+	}
+	
+	private Boid2D[] closeBoids(ArrayList<Boid2D> boids)
+	{
+		Boid2D[] c = new Boid2D[0];
+		return boids.toArray(c);
+	}
+	
+	//Calculates the seekForce for the center of all close boids.
+	private Point2D seek(Boid2D[] boids)
+	{
+		
+		return seekForce;
+	}
+	
+	//Calculates the flee force from predators and boids that are too close.
+	private Point2D flee(Boid2D[] boids)
+	{
+		
+		return fleeForce;
+	}
+	
+	//Calculates the force of avoiding obstacles in the way of the boid.
+	private Point2D avoid(Boid2D[] boids)
+	{
+		
+		return avoidForce;
+	}
+	
+	//Calculates how much the boid will randomly wander this update.
+	/*
+	 * This function uses a "circle" in front of the boid represented by vectors to change the velocity of
+	 * the boid over time. 
+	 */
+	private final double circleScale = 2;
+	private final double circleRadius = 1;
+	private Point2D wander()
+	{
+		Point2D circleCenter = velocity.normalize().multiply(circleScale);
+		Point2D circleDisplacement = new Point2D(Math.cos(Math.toRadians(wanderAngle)), Math.sin(Math.toRadians(wanderAngle)));
+		
+		wanderAngle += ((Math.random() * angleChange) - (angleChange * .5));
+		
+		wanderForce = circleCenter.add(circleDisplacement);
+		
+		return wanderForce;
+	}
+	
+	
+	
+	/*
+	 * averageVelocities will average an array of velocities by adding them all together and 
+	 * dividing by the total number of velocities. Used in the seek behavior to help align the boids to eachother.
+	 */
+	public Point2D averageVelocities(Point2D[] velocities)
+	{
+		Point2D ave = Point2D.ZERO;
+		for(int i = 0; i < velocities.length; i++)
+		{
+			ave = ave.add(velocities[i]);
+		}
+		return ave.multiply(1/velocities.length);
 	}
 }
--- Main.java
+++ Main.java
@@ -13,6 +13,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 
@@ -27,29 +28,26 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Group root = new Group();
-			Scene scene = new Scene(root,1024,768);
+			Scene scene = new Scene(root,1600, 900);
 			
-			//The first boid prototype. 
 			// Flock boid shape(-10, 13, 0, -12, 10, 13, 0, 7);
 			
 			ArrayList<Boid2D> boids = new ArrayList<Boid2D>(); //arraylist of all boids currently alive.
 			ArrayList<KeyCode> keys = new ArrayList<KeyCode>();//array list of keys that are currently pressed.
 			
-			for(int i = 0; i < 10; i++)
+			for(int i = 0; i < 50; i++)
 			{
-				boids.add(new Boid2D(new Polygon(-10, 13, 0, -12, 10, 13, 0, 7), Math.random() * 360, 5, 5, 3, 10, Math.random() * scene.getWidth(), Math.random() * scene.getHeight(), BoidType.Flock));
-			}
-			
-			for(Boid2D boid : boids)
-			{
-				root.getChildren().add(boid.getShape());
+				boids.add(new Boid2D(new Polygon(-10, 13, 0, -12, 10, 13, 0, 7), 8.0, 3.0, new Point2D(scene.getWidth(), scene.getHeight()),
+						new Point2D(Math.random() * 5 - 2.5, Math.random() * 5 - 2.5), new Point2D(Math.random() * scene.getWidth(), Math.random() * scene.getHeight()),
+						BoidType.Flock, Color.color(Math.random() * .5, Math.random(), Math.random() * .5)));
+				root.getChildren().add(boids.get(i).getShape());
 			}
 			
 			scene.setOnKeyPressed(new EventHandler<KeyEvent>() 
 			{
 
 				@Override
-				public void handle(KeyEvent event) 
+				public void handle(KeyEvent event)
 				{
 					if(!keys.contains(event.getCode()))
 					{
@@ -106,9 +104,6 @@ public class Main extends Application {
 				}
 			});
 			
-			Text mode = new Text();
-			
-			root.getChildren().add(mode);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
@@ -119,61 +114,7 @@ public class Main extends Application {
 				public void handle(long arg0) {
 					for(Boid2D boid : boids)
 					{
-						
-						boid.calculateMove(boids);
-						boid.move();
-						if(boid.getShape().getTranslateX() > scene.getWidth())
-						{
-							boid.getShape().setTranslateX(0);
-						}
-						if(boid.getShape().getTranslateX() < 0)
-						{
-							boid.getShape().setTranslateX(scene.getWidth());
-						}
-						if(boid.getShape().getTranslateY() > scene.getHeight())
-						{
-							boid.getShape().setTranslateY(0);
-						}
-						if(boid.getShape().getTranslateY() < 0)
-						{
-							boid.getShape().setTranslateY(scene.getHeight());
-						}
-						
-						Point2D currentVelocity = new Point2D(Math.sin(Math.toRadians(boid.getRotation())) * boid.getSpeed(),
-														   -Math.cos(Math.toRadians(boid.getRotation())) * boid.getSpeed());
-						double maxForce = 10;
-						//implement a seek steering behavior
-						if(mouseLeft)
-						{
-							Point2D desiredVelocity = new Point2D(target.getX() - boid.getShape().getTranslateX(), target.getY() - boid.getShape().getTranslateY());
-							desiredVelocity = desiredVelocity.normalize().multiply(boid.getSpeed());
-							
-							Point2D steeringForce = desiredVelocity.subtract(currentVelocity);
-							
-							steeringForce = steeringForce.multiply(1/maxForce);
-							
-							
-							boid.setDirection(currentVelocity.add(steeringForce));
-						}
-						//implement a flee steering behavior.
-						else if(mouseRight)
-						{
-							
-						}
-						//implement a wandering behavior.
-						else
-						{
-							double ANGLE_CHANGE = 5.0;
-							Point2D circleCenter = new Point2D(Math.sin(Math.toRadians(boid.getRotation())),
-														   -Math.cos(Math.toRadians(boid.getRotation())));
-							circleCenter = circleCenter.multiply(2);
-							Point2D displacement = new Point2D(0, 1);
-							displacement = displacement.multiply(1);
-							
-							
-							
-						}
-						
+						boid.move(boids);
 					}
 				}
 				
